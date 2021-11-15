2d17a085906975a8ae035167132f77e9de5e48ea
Adil <adil@fit.ba>
2021-11-15 02:18:44 +0100

    ok

---
+++ vjezbe06_angular/angularFIT6/src/app/zaposlenici/edit-zaposlenici/edit-zaposlenici.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-zaposlenici',
+  templateUrl: './edit-zaposlenici.component.html',
+  styleUrls: ['./edit-zaposlenici.component.css']
+})
+export class EditZaposleniciComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
