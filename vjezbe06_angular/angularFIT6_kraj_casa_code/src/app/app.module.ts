604001737c061ebfefc19133636c15f9052756e8
Adil <adil@fit.ba>
2021-11-24 12:55:45 +0100

    ok

--- vjezbe06_angular/angularFIT6_kraj_casa_code/src/app/app.module.ts
+++ vjezbe06_angular/angularFIT6_kraj_casa_code/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { Proba1Component } from './proba1/proba1.component';
 import { RouterModule } from '@angular/router';
 import { EditStudentComponent } from './studenti/edit-student/edit-student.component';
 import { EditZaposlenikComponent } from './zaposlenici/edit-zaposlenik/edit-zaposlenik.component';
+import { DrzavaComponent } from './drzava/drzava.component';
+import { DrzavaEditComponent } from './drzava/drzava-edit/drzava-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,15 +21,19 @@ import { EditZaposlenikComponent } from './zaposlenici/edit-zaposlenik/edit-zapo
     OpstineComponent,
     Proba1Component,
     EditStudentComponent,
-    EditZaposlenikComponent
+    EditZaposlenikComponent,
+    DrzavaComponent,
+    DrzavaEditComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'studenti', component: StudentiComponent},
+      {path: 'edit-student/:id', component: EditStudentComponent},
       {path: 'zaposlenici', component: ZaposleniciComponent},
       {path: 'opstine', component: OpstineComponent},
       {path: 'proba1', component: Proba1Component},
+      {path: 'putanju-do-drzava', component: DrzavaComponent},
     ]),
     FormsModule,
     HttpClientModule
