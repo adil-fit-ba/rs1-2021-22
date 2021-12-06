ec1facb5344d197857842b77e0feaff9d60253a3
Adil <adil@fit.ba>
2021-12-06 01:01:24 +0100

    backend preuzet sa wrd-a

---
+++ vjezbe08_backend_api/FIT_Api_Examples/FIT_Api_Examples/Helper/MyAuthTokenExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using FIT_Online_shop.EF;
+using FIT_Online_shop.EntityModels;
+using Microsoft.AspNetCore.Http;
+
+namespace FIT_Online_shop.Helper
+{
+    public static class MyAuthTokenExtension
+    {
+
+         public static KorisnickiNalog GetKorisnikOfAuthToken(this HttpContext httpContext)
+        {
+            string token = httpContext.GetMyAuthToken();
+            return GetKorisnikOfAuthToken(token);
+        }
+
+        public static KorisnickiNalog GetKorisnikOfAuthToken(string token)
+        {
+            MojDbContext db = new MojDbContext();
+
+            KorisnickiNalog korisnickiNalog = db.AutentifikacijaToken.Where(x => token != null && x.Vrijednost == token).Select(s => s.KorisnickiNalog).SingleOrDefault();
+            return korisnickiNalog;
+        }
+
+        public static string GetMyAuthToken(this HttpContext httpContext)
+        {
+            string token = httpContext.Request.Headers["MojAutentifikacijaToken"];
+            return token;
+        }
+    }
+}
---
+++ vjezbe08_backend_api/FIT_Api_Examples/FIT_Api_Examples/Modul4_Korisnici/Models/Kupac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIT_Online_shop.EntityModels
+{
+    public class Kupac  :KorisnickiNalog
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+    }
+}
